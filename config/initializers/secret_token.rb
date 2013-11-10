49623a206bfd2c2457c8264525f6f59071031a11
tiffanypeon <tiffanypeon@gmail.com>
2013-11-10 12:30:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NerdGym::Application.config.secret_key_base = 'bba061efbf24195ed4df506016f49d8d93ad67d6ddc65951177a0cbe7d87e40938e3d25a75fb57a9ea3db0389adcd2d3afd2ce5760d678f8864962aa42c98589'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NerdGym::Application.config.session_store :cookie_store, key: '_nerd-gym_session'
